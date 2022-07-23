db35ec89d20b7479cfac67b9dfc6038ecce020e9
alishhde <al.shohadaee@gmail.com>
2022-07-23 23:59:53 +0430

    Duplicate_Code added.

--- Smells Example/Duplicate_Code.py
+++ Smells Example/Duplicate_Code.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# Seyedali Shohadaalhosseini - Alishhde
+#  Where n is the multiplier and x is a list 
+def CalculateSumOfMultiplyOf(n, x): 
+    nums_list = []
+    for i in x:
+        nums_list.append(n*i)
+    return sum(nums_list)
+
+# Duplicated function
+def calculateSumOfMultiplyOf2(n, x):
+    for i in x:
+        nums += n*i
+    return nums
