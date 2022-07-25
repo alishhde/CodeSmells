abca0ee3b20c06799204cf2bb1eefc463c6f721f
alishhde <al.shohadaee@gmail.com>
2022-07-25 19:21:44 +0430

    Shotgun_Surgery Smell class completed.

--- Smells Example/Shotgun_Surgery.py
+++ Smells Example/Shotgun_Surgery.py
@@ -13,8 +13,7 @@ class Students():
         self.STID = ID
 
     def clubRegistration(self):
-        result = Boolean()
-        self.registeredList = list()
+        self.clubRegisteredList = list()
 
         if self.STAge < 18:
             print("Student cannot register to club")
@@ -25,7 +24,21 @@ class Students():
             return True
 
     def libraryRegistration(self):
-        ...
+        self.libraryRegisteredList = list()
+        if self.STAge < 18:
+            print("Student cannot register to library")
+            return False
+        else:
+            # Enter student ID to the registered list.
+            self.libraryRegisteredList.append(self.STID)
+            return True
 
     def courseRegitration(self):
-        ...
+        self.courseRegisteredList = list()
+        if self.STAge < 18:
+            print("Student cannot register to library")
+            return False
+        else:
+            # Enter student name, lastname, ID to the registered list.
+            self.courseRegisteredList.append([self.STName, self.STLname, self.STID])
+            return True
