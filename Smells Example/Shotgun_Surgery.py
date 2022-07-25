6071e1b82953888914e777a4a7c27e9097b4959a
alishhde <al.shohadaee@gmail.com>
2022-07-25 19:29:20 +0430

    Shotgun_Surgery smell and its solution finished.

--- Smells Example/Shotgun_Surgery.py
+++ Smells Example/Shotgun_Surgery.py
@@ -1,9 +1,6 @@
 # Seyedali Shohadaalhosseini - Alishhde
-from tkinter.font import BOLD
-from unittest import result
-from xmlrpc.client import Boolean, boolean
 
-# Problem
+# Smell Found
 class Students():
     def __init__(self, name, Lname, age, subject, ID) -> None:
         self.STName = name
@@ -37,6 +34,52 @@ class Students():
         self.courseRegisteredList = list()
         if self.STAge < 18:
             print("Student cannot register to library")
+            return False
+        else:
+            # Enter student name, lastname, ID to the registered list.
+            self.courseRegisteredList.append([self.STName, self.STLname, self.STID])
+            return True
+
+# Smell Solved
+class Students():
+    def __init__(self, name, Lname, age, subject, ID) -> None:
+        self.STName = name
+        self.STLname = Lname
+        self.STAge = age
+        self.STSubject = subject
+        self.STID = ID
+    
+    def StudentageUnderThreshold(self, Threshold=18):
+        if self.STAge < Threshold:
+            print("Student cannot register to club")
+            return True
+        # Student have permission to register.
+        return False
+
+    def clubRegistration(self):
+        self.clubRegisteredList = list()
+        if self.StudentageUnderThreshold():
+            print("Student cannot register to club")
+            return False
+        else:
+            # Enter student name to the registered list.
+            self.registeredList.append([self.STName, self.STLname])
+            return True
+
+    def libraryRegistration(self):
+        self.libraryRegisteredList = list()
+        if self.StudentageUnderThreshold():
+            print("Student cannot register to club")
+            return False
+        else:
+            # Enter student ID to the registered list.
+            self.libraryRegisteredList.append(self.STID)
+            return True
+
+    def courseRegitration(self):
+        self.courseRegisteredList = list()
+        if self.StudentageUnderThreshold():
+            print("Student cannot register to club")
             return False
         else:
             # Enter student name, lastname, ID to the registered list.
