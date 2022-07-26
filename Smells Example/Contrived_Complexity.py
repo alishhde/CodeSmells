62ff929b7424308197d98335e066ebe47678b0bc
alishhde <al.shohadaee@gmail.com>
2022-07-26 20:47:48 +0430

    Contrived_Complexity finished.

---
+++ Smells Example/Contrived_Complexity.py
@@ -0,0 +1,12 @@
+# Seyedali Shohadaalhosseini - Alishhde
+
+# smell code
+def summationOf(x):
+    """ Where x is a list of numbers. """
+    s = 0 
+    for number in x:
+        s += number
+    return s
+
+# smell solved with one line of coding
+s = sum(a)
