34d3b54ace065e3f82924598fedebfd0cfa74d96
alishhde <al.shohadaee@gmail.com>
2022-07-27 20:20:35 +0430

    Large_Class updated.

--- Smells Example/Large_Class.py
+++ Smells Example/Large_Class.py
@@ -1,1 +1,10 @@
 # Seyedali Shohadaalhosseini - Alishhde
+
+class Game():
+    # too many variables
+
+    def __init__(self) -> None:
+        # Too many attributes
+        ...
+
+    # too many methods
