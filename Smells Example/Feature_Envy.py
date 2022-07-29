1258b6977bc73b16f5bb15470e7d9c387a684a86
alishhde <al.shohadaee@gmail.com>
2022-07-29 17:41:03 +0430

    Feature_Envy smell completed.

--- Smells Example/Feature_Envy.py
+++ Smells Example/Feature_Envy.py
@@ -1,1 +1,18 @@
 # Seyedali Shohadaalhosseini - Alishhde
+
+class Animals:
+    def __init__(self, name, age, family, address):
+        self.name = name
+        self.age = age
+        self.family = family
+        self.address = address
+
+class Dog(Animals):
+    def __init__(self, name, age, family, address):
+        super().__init__(name, age, family, address)
+    
+    def creatID(self):
+        return str(self.family) + str(self.name) + str(self.age)
+
+    def __str__(self) -> str:
+        return f"Its name is {self.name}"
