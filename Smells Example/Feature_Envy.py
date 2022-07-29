bce1fd5de53f86dd99834f8d66d7ce73cd34d6fd
alishhde <al.shohadaee@gmail.com>
2022-07-29 17:44:06 +0430

    Feature_Envy solution added.

--- Smells Example/Feature_Envy.py
+++ Smells Example/Feature_Envy.py
@@ -1,5 +1,6 @@
 # Seyedali Shohadaalhosseini - Alishhde
 
+# Feature envy smell
 class Animals:
     def __init__(self, name, age, family, address):
         self.name = name
@@ -13,6 +14,24 @@ class Dog(Animals):
     
     def creatID(self):
         return str(self.family) + str(self.name) + str(self.age)
+
+    def __str__(self) -> str:
+        return f"Its name is {self.name}"
+
+# Feature envy solution
+class Animals:
+    def __init__(self, name, age, family, address):
+        self.name = name
+        self.age = age
+        self.family = family
+        self.address = address
+    
+    def creatID(self):
+        return str(self.family) + str(self.name) + str(self.age)
+        
+class Dog(Animals):
+    def __init__(self, name, age, family, address):
+        super().__init__(name, age, family, address)
 
     def __str__(self) -> str:
         return f"Its name is {self.name}"
