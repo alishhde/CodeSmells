567fe374a88c040e9bb363a3a99ebb7384acc180
alishhde <al.shohadaee@gmail.com>
2022-07-30 21:20:14 +0430

    update.

--- Smells Example/Divergent_Code.py
+++ Smells Example/Divergent_Code.py
@@ -1,10 +1,17 @@
 # Seyedali Shohadaalhosseini - Alishhde
 
+# Problem
 class Shop():
-    def __init__(self, category, quantity, priceRange):
-        self.category = category
-        self.quantity = quantity
-        self.priceRange = priceRange
+    def __init__(self, category, product, priceRange):
+        self.category = str(category) 
+        self.quantity = str(product)
+        self.priceRange = float(priceRange)
 
     def addCategory(self):
         ...
+    
+    def findRequested(self, findme):
+        ...
+
+
+# Solution
