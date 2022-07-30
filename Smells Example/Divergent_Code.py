0b5c57c5cc86ca0326d9266def6c206a277e88d1
alishhde <al.shohadaee@gmail.com>
2022-07-30 21:30:04 +0430

    Divergent_Code finished.

--- Smells Example/Divergent_Code.py
+++ Smells Example/Divergent_Code.py
@@ -4,7 +4,7 @@
 class Shop():
     def __init__(self, category, product, priceRange):
         self.category = str(category) 
-        self.quantity = str(product)
+        self.product = str(product)
         self.priceRange = float(priceRange)
 
     def addCategory(self):
@@ -15,3 +15,16 @@ class Shop():
 
 
 # Solution
+class Shop():
+    def __init__(self, category, product, priceRange, quantity):
+        self.category = str(category) 
+        self.product = str(product)
+        self.priceRange = float(priceRange)
+        self.quantity = str(int(quantity)) # Saving with string type
+
+    def addCategory(self):
+        ...
+    
+    def findRequested(self, findme):
+        ...
+
