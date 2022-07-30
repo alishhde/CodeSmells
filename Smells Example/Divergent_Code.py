f9a11fb2da2988ce13133a80263d88fc0f83c024
alishhde <al.shohadaee@gmail.com>
2022-07-30 21:17:32 +0430

    Divergent_Code started.

---
+++ Smells Example/Divergent_Code.py
@@ -0,0 +1,10 @@
+# Seyedali Shohadaalhosseini - Alishhde
+
+class Shop():
+    def __init__(self, category, quantity, priceRange):
+        self.category = category
+        self.quantity = quantity
+        self.priceRange = priceRange
+
+    def addCategory(self):
+        ...
