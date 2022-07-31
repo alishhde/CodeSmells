c5e9f65d7830f48f298b378858a787f3f089feb6
alishhde <al.shohadaee@gmail.com>
2022-07-31 15:33:45 +0430

    Problem finished.

--- Smells Example/Data_Clump.py
+++ Smells Example/Data_Clump.py
@@ -1,1 +1,10 @@
 # Seyedali Shohadaalhosseini - Alishhde 
+
+# Problem
+class Booking:
+    def __init__(self, bookingID, RoomID, From, To) -> None:
+        self.BookingID = bookingID
+        self.RoomID = RoomID
+        self.From = From
+        self.To = To
+        timeInterval = self.To - self.From
