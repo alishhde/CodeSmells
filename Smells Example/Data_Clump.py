dae9cfae72279adf2ebb6047b744d82f6819be18
alishhde <al.shohadaee@gmail.com>
2022-07-31 15:36:57 +0430

    Finished.

--- Smells Example/Data_Clump.py
+++ Smells Example/Data_Clump.py
@@ -1,6 +1,5 @@
 # Seyedali Shohadaalhosseini - Alishhde 
 
-# Problem
 class Booking:
     def __init__(self, bookingID, RoomID, From, To) -> None:
         self.BookingID = bookingID
@@ -8,3 +7,16 @@ class Booking:
         self.From = From
         self.To = To
         timeInterval = self.To - self.From
+
+# Solution
+class TimeInterval():
+    def __init__(self, From, To) :
+        self.From = From
+        self.To = To
+        return self.To - self.From
+
+class Booking:
+    def __init__(self, bookingID, RoomID, From, To) -> None:
+        self.BookingID = bookingID
+        self.RoomID = RoomID
+        self.timeInterval = TimeInterval()
