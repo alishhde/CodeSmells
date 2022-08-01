05a67aef318ff126a90dc5e791f37607c3d379ac
alishhde <al.shohadaee@gmail.com>
2022-08-01 18:51:38 +0430

    Solution added.

--- Smells Example/Inappropriate_Intimacy.py
+++ Smells Example/Inappropriate_Intimacy.py
@@ -1,6 +1,9 @@
 # Seyedali Shohadaalhosseini - Alishhde 
 
 # Problem
+from http import client
+
+
 class Info():
     def __init__(self, name, ID, Date, Address) -> None:
         self.name = name
@@ -17,3 +20,21 @@ class Cient(Info):
         print(self.ID)
         print(self.Date)
         print(self.Address)
+
+# Solution
+class Info():
+    def __init__(self, name, ID, Date, Address) -> None:
+        self.name = name
+        self.ID = ID
+        self.Date = Date
+        self.Address = Address # address is a dictionary e.g. {"Country": "IRAN", "City":"Babolsar", "Other":"Talebi St. No. 324"}
+    
+    def clientInfo(self):
+        return self.name, self.ID, self.Date, self.Address
+
+class Cient(Info):
+    def __init__(self, name, ID, Date, Address) -> None:
+        super().__init__(name, ID, Date, Address)
+    
+    def printClientInfo(self):
+        print(self.clientInfo(), sep="\n")
