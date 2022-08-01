1304cc5e085f7025b4e512a718890b366e45b31d
alishhde <al.shohadaee@gmail.com>
2022-08-01 18:47:29 +0430

    Problem Done.

---
+++ Smells Example/Inappropriate_Intimacy.py
@@ -0,0 +1,19 @@
+# Seyedali Shohadaalhosseini - Alishhde 
+
+# Problem
+class Info():
+    def __init__(self, name, ID, Date, Address) -> None:
+        self.name = name
+        self.ID = ID
+        self.Date = Date
+        self.Address = Address # address is a dictionary e.g. {"Country": "IRAN", "City":"Babolsar", "Other":"Talebi St. No. 324"}
+
+class Cient(Info):
+    def __init__(self, name, ID, Date, Address) -> None:
+        super().__init__(name, ID, Date, Address)
+    
+    def printClientInfo(self):
+        print(self.name)
+        print(self.ID)
+        print(self.Date)
+        print(self.Address)
