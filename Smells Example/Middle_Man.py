4054f85fb31da847fb54e73c9cc16d83b98e4618
alishhde <al.shohadaee@gmail.com>
2022-08-02 20:39:32 +0430

    Problem Added.

--- Smells Example/Middle_Man.py
+++ Smells Example/Middle_Man.py
@@ -1,5 +1,24 @@
 # Seyedali Shohadaalhosseini - Alishhde 
 
 # Problem
+class Client:
+    def __init__(self) -> None:
+        ...
+
+    def zipc():
+        return ZipCode.zipCode()
+
+class ZipCode:
+    def __init__(self) -> None:
+        ...
+    
+    def zipCode():
+        return "A8Sj093L"
+
+class Application:
+    def __init__(self) -> None:
+        ...
+        
+    applicationZipCode = Client.zipc()
 
 # Solution
